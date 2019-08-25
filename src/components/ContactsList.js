0dbd0600b8e760797f9cc3fc7207a1644c583329
mariana-martins <mariana.menezes@kathmandu.co.nz>
2019-08-25 18:02:25 +1200

    Fix mobile layout on contact list

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Button, List, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, Typography, Divider } from '@material-ui/core';
+import { IconButton, Button, List, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, Typography, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import indigo from '@material-ui/core/colors/indigo';
 import DeleteDialog from './DeleteDialog';
 
 const useContactItemsStyles = makeStyles({
+  itemGutters: {
+    padding: 0,
+  },
   textSecondary: {
     '& span': {
       display: 'block',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+    },
+  },
+  avatarFlex: {
+    margin: 'auto',
+  },
+  button:{
+    '&:hover': {
+      backgroundColor: indigo[100],
     },
   },
 });
@@ -19,11 +34,18 @@ function ContactItem({ entry, toggleFavorite, children }) {
   const classes = useContactItemsStyles();
   return (
     <Route key={name} render={({ history }) => (
-      <ListItem alignItems="flex-start" onClick={() => history.push(`/edit/${slug}`)}>
-        <ListItemAvatar>
-          <Button onClick={(e) => toggleFavorite(e, slug, name)}>
+      <ListItem
+        alignItems="flex-start"
+        onClick={() => history.push(`/edit/${slug}`)}
+        classes={{ gutters: classes.itemGutters }}
+      >
+        <ListItemAvatar classes={{ alignItemsFlexStart: classes.avatarFlex }}>
+          <IconButton
+            onClick={(e) => toggleFavorite(e, slug, name)}
+            className={classes.button}
+          >
             {favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-          </Button>
+          </IconButton>
         </ListItemAvatar>
         <ListItemText
           classes={{ secondary: classes.textSecondary }}
